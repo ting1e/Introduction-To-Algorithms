a93eefb7e559c6adadbb52d46ba3d60ab5b01cff
SlyLi <slylicn@qq.com>
2018-03-03 16:16:26 +0800

    第二章前两节

--- IntroductionToAlgorithms/第二章 算法基础/02_1_插入排序.h
+++ IntroductionToAlgorithms/第二章 算法基础/02_1_插入排序.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "..\bash.h"
 /* 
 插入排序思想：
 		数组中下标 i 表示当前排序的元素，
@@ -14,7 +14,7 @@ void InsertSort(int *array, int length)
 	{
 		key = array[i];
 		j = i - 1;
-		while ((j >= 0)&&(array[j]>key))
+		while ((j >= 0)&&(array[j]>key)) //练习2.1-2  改成 array[j]<key
 		{
 			array[j + 1] = array[j--];
 		}
@@ -22,6 +22,58 @@ void InsertSort(int *array, int length)
 	}
 }
 
+//插入排序简单测试
+void InsertSortTest()
+{
+	int a[5] = { 1,4,5,2,3 };
+	InsertSort(a, 5);
+}
 
+//练习2.1 - 3
+void FindV(int array[], int length,int v)
+{
+	int i;
+	for (i = 0; i < length; i++)
+	{
+		if (array[i] == v)
+		{
+			return;
+		}
+	}
+	if (i == length)
+		v = -1;	// v未出现赋值为特殊值
+}
 
+//练习2.1 - 4
+void TwoBinNumAdd(int first[], int second[],int sum[] ,int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		sum[i + 1] = first[i] + second[i];
+	}
+	for (int i = length; i > 0; i--)
+	{
+		if (sum[i] >= 2)
+		{
+			sum[i] -= 2;
+			sum[i - 1] += 1;
+		}
+	}
+}
+// 练习2.1 - 4 测试
+void TwoBinNumAddTest()
+{
+	int first[4] = { 1,1,0,1 };
+	int second[4] = { 1,1,0,0 };
+	int sum[5] = { 0 };
+	TwoBinNumAdd(first, second, sum, 5);
+}
+/*
+	练习2.1-1	A{31,41,59,26,41,58}	
+				A{31,41,59,26,41,58}	i=1
+				A{31,41,59,26,41,58}		2
+				A{26,31,41,59,41,58}		3
+				A{26,31,41,41,59,58}		4
+				A{26,31,41,41,58,59}		5
+*/
 
---
+++ IntroductionToAlgorithms/第二章 算法基础/02_2_分析算法.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "..\bash.h"
+
+/*
+	选择排序思想：
+	首先找出数组中最小元素与第一个元素进行交换，
+	接着找出次小元素与第二个元素交换，
+	依次进行，直到完成排序
+
+	循环次数是数组元素个数-1，依次寻找各个元素后，
+	最后剩下的就是就是最大的元素。保存在最后的位置。
+*/
+
+//练习2.2 - 2 选择算法
+void SelectionSort(int array[], int length)
+{
+	for (int i = 0; i < length-1; i++)
+	{
+		int min = array[i];
+		int min_index = i;
+		for (int j = i + 1; j < length; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				min_index = j;
+			}
+		}
+		int temp = array[i];
+		array[i] = min;
+		array[min_index] = temp;
+	}
+}
+
+//直接选择排序简单测试
+void SelectionSortTest()
+{
+	int a[5] = { 1,4,5,2,3 };
+	SelectionSort(a, 5);
+}
+
+/*
+练习2.3 - 3
+	设数组长度为n
+	average:		(1+2+3+4+...+n)/2=(n+1)/2
+	worset(max) :	n
+*/
