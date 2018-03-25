d1c0ef05ac05fe3f25fc031b2b9f8a6ef4bb99bb
SlyLi <slylicn@qq.com>
2018-03-25 21:46:01 +0800

    桶排序

--- IntroductionToAlgorithms/第八章 线性时间排序/08_2_计数排序.h
+++ IntroductionToAlgorithms/第八章 线性时间排序/08_2_计数排序.h
@@ -37,12 +37,51 @@ void CountSort(int a[], int b[],int size, int k)
 	free(c);
 }
 
+void MyCountSort(int a[],int size)
+{
+	int i;
+	int max = a[0];
+	for (i = 1; i < size; i++)
+	{
+		max = max > a[i] ? max : a[i];
+	}
+
+	int *b = (int *)malloc(sizeof(int)*size);
+	int *c = (int *)malloc(sizeof(int)*(max+1));	
+	for (i = 0; i < max+1; i++)
+	{
+		c[i] = 0;
+	}
+	for (i = 0; i < size; i++)
+	{
+		c[a[i]] += 1;
+	}
+	for (int i = 1; i < max+1; i++)
+	{
+		c[i] = c[i] + c[i - 1];
+	}
+
+	for (int j = size - 1; j >= 0; j--)
+	{
+		b[c[a[j]] - 1] = a[j];
+		c[a[j]]--;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		a[i] = b[i];
+	}
+
+	free(b);
+	free(c); 
+}
+
 
 void CountSortTest()
 {
 	int a[] = { 2,5,3,0,2,3,0,3 };
 	int b[8];
-	CountSort(a, b, 8, 10);
+	MyCountSort(a,8);
 }
 
 /*
---
+++ IntroductionToAlgorithms/第八章 线性时间排序/08_4_桶排序.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include "..\base.h"
+
+typedef struct ELEMENT
+{
+	double x;
+	struct ELEMENT *next;
+}Element;
+
+void InsertAnElement(Element *e, double a)	//链表尾部插入一个数据
+{
+	while (e->next)
+		e = e->next;
+	Element *new_one = (Element *)malloc(sizeof(Element));
+	new_one->next = NULL;
+	new_one->x = a;
+	e->next = new_one;
+}
+
+void InsertSort(Element *e) //链表插入排序
+{
+	if (e->next == NULL)
+		return;
+	Element *current = e->next->next;
+	e->next->next = NULL;
+	while (current)
+	{
+		Element *target = e;
+		while (target->next!=NULL&&target->next->x<current->x)
+		{
+			target = target->next;
+		}
+		Element *temp = current;
+		current = current->next;
+		temp->next = target->next;
+		target->next = temp;
+	}
+}
+
+void BucketSort(double a[], int size)
+{
+	Element *b = (Element *)malloc(sizeof(Element)*size);
+	for (int i = 0; i < size; i++)
+	{
+		b[i].next = NULL;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		InsertAnElement(&b[(int)(a[i] * size)], a[i]);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		InsertSort(&b[(int)(a[i] * size)]);
+	}
+	int j = 0;
+	for (int i = 0; i < size; i++)
+	{
+		Element *temp = b[i].next;
+		while (temp!=NULL)
+		{
+			a[j++] = temp->x;
+			temp = temp->next;
+		}
+	}
+}
+
+void BucketSortTest()
+{
+	double a[] = { 0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.12,0.23,0.68 };
+	BucketSort(a,10);
+
+}
+
+/*
+	练习 8.4-2 当所有数都在一个区间时，因插入排序运行时间为Θ(n²) 故。。。
+		修改链表排序的方式，此排序方法最坏情况下的时间代价为O(n lgn)
+		
+	练习 8.4-3 
+	X=0	0.25	
+	X=1 0.5
+	X=2 0.25
+	E(X)=1
+	E²(X)=1
+	E(X²)=1.5
+
+	练习 8.4-3  r∈(0,1) r分成n段,分别为
+	0 - √(1/n),√(1/n) - √(2/n),√(2/n) - √(3/n),.........√((n-1)/n) - 1
+*/
